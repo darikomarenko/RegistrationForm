09f1e90f76a05afb12d246ca50516943aa5b6886
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2023-11-20 14:07:05 +0300

    fix error display

--- index.js
+++ index.js
@@ -7,82 +7,60 @@ const profession = form.elements.profession;
 const password = form.elements.password;
 const password2 = form.elements.password2;
 
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  clearErrors();
-  checkAll();
-  checkCheckboxValidity();
-  showData();
-  //form.reset();
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    const inputs = getInputs(form);
+
+    clearErrors();
+    if (isFormValid(inputs)) {
+        outputData(inputs);
+        form.reset();
+    }
 });
 
-function checkValidity(input) {
-  let validity = input.validity;
-  let errors = [];
-
-  if (validity.patternMismatch) {
-    input.nextElementSibling.textContent = input.validationMessage;
-  }
-
-  if (validity.rangeOverflow) {
-    input.nextElementSibling.textContent = input.validationMessage;
-  }
-
-  if (validity.rangeUnderflow) {
-    input.nextElementSibling.textContent = input.validationMessage;
-  }
-
-  if (validity.tooLong) {
-    input.nextElementSibling.textContent = input.validationMessage;
-  }
-  if (validity.valueMissing) {
-    input.nextElementSibling.textContent = input.validationMessage;
-  }
-  return errors;
+function getInputs(form) {
+    const inputs = [];
+    inputs.push(...form.querySelectorAll('input'));
+    inputs.push(...form.querySelectorAll('select'));
+    return inputs;
 }
 
-function showData(errors) {
-  if (errors === "") {
-    console.log(input.value);
-    return false;
-  }
-}
-
-function checkCheckboxValidity() {
-  let checkbox = document.getElementById("agree");
-  if (!checkbox.checked) {
-    checkbox.nextElementSibling.textContent = checkbox.validationMessage;
-  }
-}
-
-//Проверка для всех полей
-function checkAll() {
-  //Получаем все инпуты
-  let inputs = document.querySelectorAll("input");
-  for (let input of inputs) {
-    let errors = checkValidity(input) || [];
-    if (errors) {
-      input.classList.add("error");
-      input.insertAdjacentText("afterend", errors.join("\n"));
+function outputData(inputs) {
+    const data = {};
+    for (let input of inputs) {
+        data[input.name] = input.value;
     }
-  }
+    console.log(data);
+}
+
+function isFormValid(inputs) {
+    let isValid = true;
+
+    for (let input of inputs) {
+        const errorMsg = input.validationMessage;
+        if (errorMsg) {
+            isValid = false;
+
+            input.classList.add('error');
+
+            // для radio input должна выводиться только одна ошибка
+            const alreadyHasError = input.parentElement.getElementsByClassName('error-message').length > 0;
+            if (alreadyHasError) {
+                continue;
+            }
+
+            const errorMsgElement = document.createElement('span');
+            errorMsgElement.innerText = errorMsg;
+            errorMsgElement.classList.add('error-message');
+            input.parentElement.insertAdjacentElement('beforeend', errorMsgElement);
+        }
+    }
+    return isValid;
 }
 
 function clearErrors() {
-  // Очищаем сообщения об ошибках
-  let errorMessages = document.getElementsByClassName("error");
-  for (let i = 0; i < errorMessages.length; i++) {
-    errorMessages[i].innerHTML = "";
-  }
-
-  // Удаляем классы ошибок у полей и checkbox
-  let errorFields = document.getElementsByClassName("error");
-  for (let i = 0; i < errorFields.length; i++) {
-    errorFields[i].classList.remove("error");
-  }
-
-  let errorCheckbox = document.getElementsByClassName("checkbox-error");
-  for (let i = 0; i < errorCheckbox.length; i++) {
-    errorCheckbox[i].classList.remove("checkbox-error");
-  }
+    const errors = document.getElementsByClassName('error-message');
+    for (var i = errors.length - 1; i >= 0; i--) {
+        errors[i].remove();
+    }
 }
